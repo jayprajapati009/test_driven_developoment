1d3302f70612581dbcdd1feb9f959dcaa1202975
joshuag1214 <joshuag1214@gmail.com>
2022-09-30 15:24:31 -0400

    adding cpp boiler template structure

---
+++ app/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//#include <lib.hpp>
+
+/**
+ * @file main.cpp
+ * @author Joshua Gomes (GitID:joshuag1214),Venkata Sairam Polina (GitID:sairampolina)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-29
+ * //TODO stub
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * @brief Computes a new velocity given target set point and actual velocity
+ * 
+ */
+
+int main(){
+
+    return 0;
+}
